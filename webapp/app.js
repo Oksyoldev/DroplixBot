425a9914b290e5de19df81472c17ed674856569e
Никита <n.chetenov@gmail.com>
2025-06-19 13:24:09 +0300

    хз

---
+++ webapp/app.js
@@ -0,0 +1,15 @@
+document.getElementById("openCase").addEventListener("click", async () => {
+  const resultBox = document.getElementById("result");
+
+  resultBox.innerText = "Открываем кейс... 🎲";
+
+  try {
+    const response = await fetch("https://droplixbot.onrender.com/api/open-case");
+    const data = await response.json();
+    resultBox.innerText = `🔥 Вам выпало: ${data.prize}`;
+  } catch (err) {
+    resultBox.innerText = "❌ Ошибка при открытии кейса";
+  }
+});
+
+window.Telegram.WebApp.ready();
